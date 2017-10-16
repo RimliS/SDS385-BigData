03b0432294f8dfe24f534314109b81dfe60b4a7e
RimliS <31521188+RimliS@users.noreply.github.com>
2017-10-15 22:03:47 -0500

    Create Ex5Part2A.R

---
+++ Exercise-5/Ex5Part2A.R
@@ -0,0 +1,49 @@
+#####################################################################################
+##Part LASSO
+
+##Import dataset containing predictors:
+diabetesX <- read.table("/Users/rimlisengupta/Documents/SDS385/Ex-5-Data/DiabetesX.txt",
+                header=T,sep=',')
+
+##Import dataset containing response:
+diabetesY <- read.table("/Users/rimlisengupta/Documents/SDS385/Ex-5-Data/DiabetesY.txt")
+
+#####################################################################################
+#Combining predictors and response into a single dataset
+diabetes <- data.frame(diabetesX)
+diabetes$Y <- diabetesY$V1
+
+### Get libraries:
+library(glmnet)
+library(MASS)
+
+#set.seed(101)
+
+##lasso Model fit:
+lasso_fit = glmnet( x = as.matrix(diabetes[,-65]), y = diabetes$Y, alpha = 1)
+
+##Predicted values of beta:
+beta_predicted <- coef(lasso_fit,s=0.1) ##s=Shrinkage Factor: t/sum(abs(beta_hat))
+##t:tuning parameter
+
+beta_hat <- as.matrix(beta_predicted)
+beta_hat <- beta_hat[,1]
+
+##Pot
+plot(lasso_fit, xvar ="lambda")
+mtext("Beta_hat vs log(lambda)", side=3, line=2, cex.lab=1,las=1, col="blue")
+
+##Predicted value of y:
+lasso_pred_y = predict(lasso_fit, newx =as.matrix(diabetes[,-65]), s=c(0.01,0.1))
+tab <- cbind(diabetes$Y,lasso_pred_y)
+
+##MSE
+mse = function(a,b) (mean((a-b)^2)) #function defined for MSE
+
+##MSE for s=0.01
+meansq1 <- mse(tab[,1], tab[,2])
+##2433.243
+
+##MSE for s=0.1
+meansq2 <- mse(tab[,1], tab[,3])
+#2454.799
