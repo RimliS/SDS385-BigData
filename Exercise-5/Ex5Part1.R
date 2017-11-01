0328e9abd4bd0f8733243a9fbbe60f91bb1c7cc9
RimliS <31521188+RimliS@users.noreply.github.com>
2017-11-01 18:48:05 -0500

    Update Ex5Part1.R

--- Exercise-5/Ex5Part1.R
+++ Exercise-5/Ex5Part1.R
@@ -39,41 +39,46 @@ plot(y,S_y(y,lambda),ty="l",main="lambda=10")
 ##Part B Soft Thresholding
 ##################################################################################
 
-set.seed(123) ##Fixing seed
+set.seed(4001) ##Fixing seed
 
 ##Case-1: sigma^2 are all equal
 #################################################################################
 theta <- c(rep(2,5),rep(-2,5),rep(0,90)) ##Creating a sparse vector
 sigma <- rep(0.2,100) ##Assuming sigma^2s are all equal
-z <- list(mode="vector",length=length(theta)) ##Initialize the data vector
-
+#z <- list(mode="vector",length=length(theta)) ##Initialize the data vector
+z <- rep(0,length(theta))
 
 ##Loop for simulating one data point for each theta:
 for (i in 1:length(theta)){
   z[i] <- rnorm(1,theta[i],sigma[i])
 }
 
-z<-unlist(z, use.names=FALSE) ##converting from list to vector
+#z<-unlist(z, use.names=FALSE) ##converting from list to vector
 
 theta_y <- function(y, lambda) {  # Soft Thresholding Operator
   return((abs(y) - lambda) * sign(y) * (abs(y) > lambda))
 }
 
-lambda <- 5 ##Initialize lambda value
+
+##Plotting predicted value of theta versus theta
+plot(1:length(theta),theta_y(y=z, lambda=1.5*(sigma^2)),ty="l",col="red",
+     ylab="Predicted theta",xlab="theta",main="Sparsity check with different lambda")
+lines(1:length(theta),theta_y(y=z, lambda=2*(sigma^2)),ty="l",col="blue")
+lines(1:length(theta),theta_y(y=z, lambda=4*(sigma^2)),ty="l",col="green")
+legend('topright',c("lambda=1","lambda=2","lambda=4"),
+       col=c("red","blue","green"),lty=1:2,lwd=1,cex = 0.5)
+
+
+lambda <- 1.5 ##Initialize lambda value
 
 ##Predicted value of theta for different lambda values:
 theta_yi <- theta_y(y=z, lambda=lambda*(sigma^2))
 
-
-##Plotting predicted value of theta versus theta
-plot(1:length(theta),theta_yi,ty="h",main="lambda=5")
-
-##Lambda=5
 ###Mean-squared error:
 MSE_lambda <- (sum((theta_yi-theta)^2))/length(theta)
 
 ##MSE for different values of lambda:
-vary.lambda <- seq(0,50,len=100)
+vary.lambda <- seq(0,5,len=100)
 MSE_lambda <- NULL
 
 for (j in 1:length(vary.lambda)) {
@@ -81,7 +86,7 @@ for (j in 1:length(vary.lambda)) {
 }
 
 ##Plotting of MSE for different lambda values:
-plot(vary.lambda,MSE_lambda,ty="h")
+plot(vary.lambda,MSE_lambda,ty="l",main="MSE vs lambda")
 #####################################################################################
 
 ##Case-2: sigma^2 are not equal
@@ -89,8 +94,8 @@ plot(vary.lambda,MSE_lambda,ty="h")
 set.seed(123) ##Fixing seed
 
 theta <- c(rep(2,5),rep(-2,5),rep(0,90)) ##Creating a sparse vector
-sigma <- runif(100,1,10) ##sigma^2s are not equal
-z <- list(mode="vector",length=length(theta)) ##Initialize the data vector
+sigma <- runif(100,1,10)
+z <- rep(0,length(theta)) ##Initialize the data vector
 
 
 ##Loop for simulating one data point for each theta:
@@ -98,7 +103,6 @@ for (i in 1:length(theta)){
   z[i] <- rnorm(1,theta[i],sigma[i])
 }
 
-z<-unlist(z, use.names=FALSE) ##converting from list to vector
 
 theta_y <- function(y, lambda) {  # Soft Thresholding Operator
   return((abs(y) - lambda) * sign(y) * (abs(y) > lambda))
@@ -109,11 +113,6 @@ lambda <- 0.5 ##Initialize lambda value
 ##Predicted value of theta for different lambda values:
 theta_yi <- theta_y(y=z, lambda=lambda*(sigma^2))
 
-
-##Plotting predicted value of theta versus theta
-plot(1:length(theta),theta_yi,ty="l",main="lambda=0.5")
-
-##Lambda=0.5
 ###Mean-squared error:
 MSE_lambda <- (sum((theta_yi-theta)^2))/length(theta)
 
